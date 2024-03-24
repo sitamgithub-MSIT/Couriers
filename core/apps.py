7e06335b45fa24942a1c937b53d214598cc889d0
Sitam <sitammeur@gmail.com>
2024-03-24 10:25:53 +0530

    core app config docs added

--- core/apps.py
+++ core/apps.py
@@ -2,5 +2,13 @@ from django.apps import AppConfig
 
 
 class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
+    """
+    AppConfig for the 'core' app.
+
+    This class represents the configuration for the 'core' app in Django.
+    It sets the default auto field to 'django.db.models.BigAutoField'
+    and specifies the name of the app as 'core'.
+    """
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "core"
