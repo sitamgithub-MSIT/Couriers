a706d14f54fbc1d62299be5aab8f68448d8607b2
Sitam <sitammeur@gmail.com>
2024-03-24 17:52:28 +0530

    static files and base templte added

--- core/views.py
+++ core/views.py
@@ -14,3 +14,29 @@ def homepageview(request):
         A rendered HTML response for the homepage.
     """
     return render(request, "core/home.html")
+
+
+def customerpageview(request):
+    """
+    Renders the customer page view.
+
+    Args:
+        request: The HTTP request object.
+
+    Returns:
+        A rendered HTML response for the customer page.
+    """
+    return render(request, "core/customer.html")
+
+
+def courierpageview(request):
+    """
+    Renders the courier page view.
+
+    Args:
+        request: The HTTP request object.
+
+    Returns:
+        A rendered HTML response for the courier page.
+    """
+    return render(request, "core/courier.html")
