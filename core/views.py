66976d120e939465c90b5ba0e97ecbc491059188
Sitam <sitammeur@gmail.com>
2024-03-24 11:10:59 +0530

    home page view added

--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def homepageview(request):
+    """
+    Renders the homepage view.
+
+    Args:
+        request: The HTTP request object.
+
+    Returns:
+        A rendered HTML response for the homepage.
+    """
+    return render(request, "core/home.html")
