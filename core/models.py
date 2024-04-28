cdf66944d082e7c9fd8fcd7c3f504bba0a2b3b76
Sitam <sitammeur@gmail.com>
2024-04-28 19:03:57 +0530

    phone number field added in model

--- core/models.py
+++ core/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to="customer/avatars/", blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True)
 
     def __str__(self):
         return self.user.get_full_name()
